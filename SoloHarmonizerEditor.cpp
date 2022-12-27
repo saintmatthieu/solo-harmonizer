6e33451b75b0f12ab0b5fee7fde9868e698d4b42
saintmatthieu <saintmatthieu@gmail.com>
2022-12-27 21:47:43 +0100

    formatting

--- SoloHarmonizerEditor.cpp
+++ SoloHarmonizerEditor.cpp
@@ -1,33 +1,31 @@
-#include "SoloHarmonizerProcessor.h"
 #include "SoloHarmonizerEditor.h"
+#include "SoloHarmonizerProcessor.h"
 
 //==============================================================================
-SoloHarmonizerEditor::SoloHarmonizerEditor (SoloHarmonizerProcessor& p)
-    : AudioProcessorEditor (&p), processorRef (p)
-{
-    juce::ignoreUnused (processorRef);
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+SoloHarmonizerEditor::SoloHarmonizerEditor(SoloHarmonizerProcessor &p)
+    : AudioProcessorEditor(&p), processorRef(p) {
+  juce::ignoreUnused(processorRef);
+  // Make sure that before the constructor has finished, you've set the
+  // editor's size to whatever you need it to be.
+  setSize(400, 300);
 }
 
-SoloHarmonizerEditor::~SoloHarmonizerEditor()
-{
-}
+SoloHarmonizerEditor::~SoloHarmonizerEditor() {}
 
 //==============================================================================
-void SoloHarmonizerEditor::paint (juce::Graphics& g)
-{
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+void SoloHarmonizerEditor::paint(juce::Graphics &g) {
+  // (Our component is opaque, so we must completely fill the background with a
+  // solid colour)
+  g.fillAll(
+      getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+  g.setColour(juce::Colours::white);
+  g.setFont(15.0f);
+  g.drawFittedText("Hello World!", getLocalBounds(),
+                   juce::Justification::centred, 1);
 }
 
-void SoloHarmonizerEditor::resized()
-{
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+void SoloHarmonizerEditor::resized() {
+  // This is generally where you'll want to lay out the positions of any
+  // subcomponents in your editor..
 }
--- SoloHarmonizerEditor.h
+++ SoloHarmonizerEditor.h
@@ -3,20 +3,19 @@
 #include "SoloHarmonizerProcessor.h"
 
 //==============================================================================
-class SoloHarmonizerEditor  : public juce::AudioProcessorEditor
-{
+class SoloHarmonizerEditor : public juce::AudioProcessorEditor {
 public:
-    explicit SoloHarmonizerEditor (SoloHarmonizerProcessor&);
-    ~SoloHarmonizerEditor() override;
+  explicit SoloHarmonizerEditor(SoloHarmonizerProcessor &);
+  ~SoloHarmonizerEditor() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
+  //==============================================================================
+  void paint(juce::Graphics &) override;
+  void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    SoloHarmonizerProcessor& processorRef;
+  // This reference is provided as a quick way for your editor to
+  // access the processor object that created it.
+  SoloHarmonizerProcessor &processorRef;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SoloHarmonizerEditor)
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SoloHarmonizerEditor)
 };
