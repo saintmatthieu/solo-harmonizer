d45d3740a822b812bc28ad6f4918bb893b834414
saintmatthieu <saintmatthieu@gmail.com>
2023-03-02 21:42:04 +0100

    Unit-test code clean-up

--- saint/IntervalGetter/IntervalGetterTests.cpp
+++ saint/IntervalGetter/IntervalGetterTests.cpp
@@ -12,40 +12,42 @@ namespace {
 float getPitch(int noteNumber) {
   return 440 * std::powf(2, (noteNumber - 69) / 12.f);
 }
-
 constexpr auto ticksPerCrotchet = 1.0;
+using OptPlayedNote = std::optional<PlayedNote>;
+constexpr OptPlayedNote noNote;
+constexpr OptPlayedNote aloneA4{{69}};
+constexpr OptPlayedNote minor3rdB4{{71, 3}};
+constexpr OptPlayedNote major3rdB4{{71, 4}};
 } // namespace
 
-TEST(IntervalGetterTests, unit_test) {
-  using OptPlayedNote = std::optional<PlayedNote>;
-  constexpr OptPlayedNote noNote;
-  constexpr OptPlayedNote aloneA4{{69}};
-  constexpr OptPlayedNote minor3rdB4{{71, 3}};
-  constexpr OptPlayedNote major3rdB4{{71, 4}};
-  IntervalGetter sut{{{0, noNote},
-                      {3, aloneA4},
-                      {5, minor3rdB4},
-                      {8, major3rdB4},
-                      {13, noNote}},
+TEST(IntervalGetter, returns_nullopt_when_tick_is_outside_boundaries) {
+  IntervalGetter sut{{
+                         {1, minor3rdB4},
+                         {2, noNote},
+                     },
                      ticksPerCrotchet};
-
-  // Anywhere within [0, 5) should return no harmony
   EXPECT_EQ(std::nullopt, sut.getHarmoInterval(0, 440.f));
+  EXPECT_EQ(3, sut.getHarmoInterval(1, 440.f));
   EXPECT_EQ(std::nullopt, sut.getHarmoInterval(2, 440.f));
-  EXPECT_EQ(std::nullopt, sut.getHarmoInterval(3, 440.f));
-  EXPECT_EQ(std::nullopt, sut.getHarmoInterval(4, 440.f));
+}
 
-  auto expectedInterval = 3;
-  EXPECT_FLOAT_EQ(*sut.getHarmoInterval(5, getPitch(71)), expectedInterval);
-  // Anything else is just linear intra/extrapolation
-  EXPECT_FLOAT_EQ(*sut.getHarmoInterval(5, getPitch(72)), expectedInterval);
-  EXPECT_FLOAT_EQ(*sut.getHarmoInterval(5, getPitch(70)), expectedInterval);
-  EXPECT_FLOAT_EQ(*sut.getHarmoInterval(5, getPitch(100)), expectedInterval);
+TEST(IntervalGetter, returns_nullopt_if_no_interval) {
+  IntervalGetter sut{{
+                         {0, aloneA4}, // not an interval, just a note by itself
+                         {1, noNote},
+                     },
+                     ticksPerCrotchet};
+  EXPECT_EQ(std::nullopt, sut.getHarmoInterval(0, 440.f));
+}
 
-  // Now harmonizing by a major 3rd
-  expectedInterval = 4;
-  EXPECT_FLOAT_EQ(*sut.getHarmoInterval(8, getPitch(71)), expectedInterval);
-  EXPECT_FLOAT_EQ(*sut.getHarmoInterval(8, getPitch(72)), expectedInterval);
-  EXPECT_FLOAT_EQ(*sut.getHarmoInterval(8, getPitch(70)), expectedInterval);
-  EXPECT_FLOAT_EQ(*sut.getHarmoInterval(8, getPitch(100)), expectedInterval);
+TEST(IntervalGetter,
+     currently_returns_constant_interval_independently_on_input_pitch) {
+  IntervalGetter sut{{
+                         {0, minor3rdB4},
+                         {1, noNote},
+                     },
+                     ticksPerCrotchet};
+  const auto expectedInterval = 3;
+  EXPECT_FLOAT_EQ(*sut.getHarmoInterval(0, getPitch(0)), expectedInterval);
+  EXPECT_FLOAT_EQ(*sut.getHarmoInterval(0, getPitch(100)), expectedInterval);
 }
