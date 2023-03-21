61f2de83be77cee762f739e0618ff7069e573d66
saintmatthieu <saintmatthieu@gmail.com>
2023-03-21 06:01:51 +0100

    Frame rate to 40 per second

--- saint/SoloHarmonizer/SoloHarmonizerVst.cpp
+++ saint/SoloHarmonizer/SoloHarmonizerVst.cpp
@@ -47,7 +47,7 @@ void SoloHarmonizerVst::_editorCallThreadFun() {
         editor->updateTimeInCrotchets(*time);
       }
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds{40});
+    std::this_thread::sleep_for(std::chrono::milliseconds{25});
   }
 }
 
