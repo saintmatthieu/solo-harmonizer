7552799c1e9cd35d8f735ba6d95243b092d2f0d0
saintmatthieu <saintmatthieu@gmail.com>
2023-03-21 06:00:35 +0100

    Slightly increase frame rate

--- saint/SoloHarmonizer/SoloHarmonizerVst.cpp
+++ saint/SoloHarmonizer/SoloHarmonizerVst.cpp
@@ -47,7 +47,7 @@ void SoloHarmonizerVst::_editorCallThreadFun() {
         editor->updateTimeInCrotchets(*time);
       }
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds{50});
+    std::this_thread::sleep_for(std::chrono::milliseconds{40});
   }
 }
 
